98c97071128751331da77c8830b7333e9f778ed2
weierliteyifujiadeng <111738937+weierliteyifujiadeng@users.noreply.github.com>
2022-08-23 22:01:03 +0800

    pull commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello1");
         System.out.println("hello2");
         System.out.println("put test!");
+        System.out.println("pull test!");
     }
 }
